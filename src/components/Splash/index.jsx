c351e99a099f6ee0bd7e82f71838e6456d1339a5
aoqlsdl <hyeeev@gmail.com>
2024-03-17 00:44:58 +0900

    🌈ui: 스플래시 추가

---
+++ src/components/Splash/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import * as s from './styles';
+
+const Splash = ({ onFadeout }) => {
+	// 애니메이션 상태 관리
+	const [fadeout, setFadeout] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setFadeOut(true);
+		}, 3000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	// 애니메이션 종료
+	const handleAnimationEnd = () => {
+		if (fadeout) {
+			onFadeout();
+		}
+	};
+	return (
+		<>
+			<s.Container
+				bg={'/imgs/bg_splash.webp'}
+				fadeout={fadeout}
+				onAnimationEnd={handleAnimationEnd}
+			>
+				<s.Wrapper>
+					<s.Title>도움배움</s.Title>
+					<s.Subtitle>나만의 이화나래 찾아보기</s.Subtitle>
+				</s.Wrapper>
+			</s.Container>
+		</>
+	);
+};
+
+export default Splash;
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -3,8 +3,19 @@ import Modal from '../../components/Modal';
 import Button from '../../components/Button';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Splash from '../../components/Splash';
 
 const LandingPage = () => {
+	// 스플래시
+	const [isLoading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	});
+
+	// 모달
 	const [isModalOpen, setModalOpen] = useState(true);
 
 	// 모달 닫기
@@ -31,61 +42,68 @@ const LandingPage = () => {
 	return (
 		<>
 			<s.Container>
-				{isModalOpen ? <Modal onClose={handleCloseModal} /> : <></>}
-				<s.Wrapper>
-					<s.Contents>
-						<s.Title>
-							날개가 되어 주실 벗들,
-							<br />
-							환영해요!
-						</s.Title>
-						<s.DescCard>
-							<s.Content>
-								<s.Head>이화나래벗이란?</s.Head>
-								<s.Body>이러저러한 활동을 하는 거에요</s.Body>
-							</s.Content>
-						</s.DescCard>
-						<s.DescCard>
-							<s.Content>
-								<s.Head>이화나래벗이 되면 이런 장점이 있어요</s.Head>
-								<s.Body>
-									<s.Li>
-										국가근로장학금으로 활동시간에 따라 급여를 받을 수 있어요
-									</s.Li>
-									<s.Li>
-										자원봉사로 신청 시, 사회봉사활동 인증제 또는 사회봉사
-										학점(1학점)으로 인정받을 수 있어요
-									</s.Li>
-									<s.Li>
-										나중에 수강 예정인 과목을 미리 공부하고, 관심 있는 타 전공
-										수업도 경험할 수 있어요
-									</s.Li>
-									<s.Li>새로운 인간관계를 만들 수 있어요 </s.Li>
-									<s.Li>
-										장애학생의 수업권 보장을 위해 직접 기여할 수 있어요
-									</s.Li>
-								</s.Body>
-							</s.Content>
-						</s.DescCard>
-						<s.SubTitle>
-							지금, 벗의 도움을 기다리는
-							<br />
-							장애학생 <s.HighLight>nn</s.HighLight>명
-						</s.SubTitle>
-						<s.Promote>나는 어떤 날개가 될까?</s.Promote>
-						<Button
-							width={20.94}
-							marginBottom={3.38}
-							rad={8}
-							font={1.25}
-							color={'#fff'}
-							bg={'#0047ff'}
-							func={handleHref}
-						>
-							매칭 시작하기
-						</Button>
-					</s.Contents>
-				</s.Wrapper>
+				{/* {isLoading && <Splash onFadeOut={} />} */}
+				{isLoading ? (
+					<Splash onFadeout={() => setLoading(false)} />
+				) : (
+					<>
+						{isModalOpen ? <Modal onClose={handleCloseModal} /> : <></>}
+						<s.Wrapper>
+							<s.Contents>
+								<s.Title>
+									날개가 되어 주실 벗들,
+									<br />
+									환영해요!
+								</s.Title>
+								<s.DescCard>
+									<s.Content>
+										<s.Head>이화나래벗이란?</s.Head>
+										<s.Body>이러저러한 활동을 하는 거에요</s.Body>
+									</s.Content>
+								</s.DescCard>
+								<s.DescCard>
+									<s.Content>
+										<s.Head>이화나래벗이 되면 이런 장점이 있어요</s.Head>
+										<s.Body>
+											<s.Li>
+												국가근로장학금으로 활동시간에 따라 급여를 받을 수 있어요
+											</s.Li>
+											<s.Li>
+												자원봉사로 신청 시, 사회봉사활동 인증제 또는 사회봉사
+												학점(1학점)으로 인정받을 수 있어요
+											</s.Li>
+											<s.Li>
+												나중에 수강 예정인 과목을 미리 공부하고, 관심 있는 타
+												전공 수업도 경험할 수 있어요
+											</s.Li>
+											<s.Li>새로운 인간관계를 만들 수 있어요 </s.Li>
+											<s.Li>
+												장애학생의 수업권 보장을 위해 직접 기여할 수 있어요
+											</s.Li>
+										</s.Body>
+									</s.Content>
+								</s.DescCard>
+								<s.SubTitle>
+									지금, 벗의 도움을 기다리는
+									<br />
+									장애학생 <s.HighLight>nn</s.HighLight>명
+								</s.SubTitle>
+								<s.Promote>나는 어떤 날개가 될까?</s.Promote>
+								<Button
+									width={20.94}
+									marginBottom={3.38}
+									rad={8}
+									font={1.25}
+									color={'#fff'}
+									bg={'#0047ff'}
+									func={handleHref}
+								>
+									매칭 시작하기
+								</Button>
+							</s.Contents>
+						</s.Wrapper>
+					</>
+				)}
 			</s.Container>
 		</>
 	);
