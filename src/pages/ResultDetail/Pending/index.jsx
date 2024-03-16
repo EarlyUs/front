2dab5c90cf9dda4d42c0279abc4ca8e732e446fb
aoqlsdl <hyeeev@gmail.com>
2024-03-16 20:48:14 +0900

    🔧fix: 폴더명 수정

---
+++ src/pages/ResultDetail/Pending/index.jsx
@@ -0,0 +1,30 @@
+import * as s from './styles';
+
+const PendingPage = () => {
+	return (
+		<>
+			<s.Container>
+				<s.Wrapper>
+					<s.Title>
+						이화나래벗으로
+						<br />
+						활동 가능한 시간이
+						<br />
+						지금은 존재하지 않아요
+					</s.Title>
+					<s.RecommendBox>
+						<s.Title>
+							그렇지만,
+							<br />
+							이화연벗의 도움이
+							<br />
+							절실히 필요한 수업이 있어요
+						</s.Title>
+					</s.RecommendBox>
+				</s.Wrapper>
+			</s.Container>
+		</>
+	);
+};
+
+export default PendingPage;
---
+++ src/pages/ResultDetail/Pending/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+	width: 21rem;
+	margin: 5.44rem auto 0;
+`;
+
+export const Title = styled.div`
+	font-size: 1.75rem;
+	font-weight: 700;
+	line-height: 2.06rem;
+`;
