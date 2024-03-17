6318994ad9b58e177166473d1cadd613f1a35d6d
aoqlsdl <hyeeev@gmail.com>
2024-03-17 15:54:45 +0900

    🔧fix: 전화번호 포맷터 수정

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,15 +3,14 @@
  * @param {string} phoneNumber - 포맷팅할 전화번호
  * @returns {string} 포맷팅된 전화번호
  */
-
-export const formatPhoneNumber = phoneNumber => {
-	// 전화번호에서 숫자가 아닌 모든 문자를 제거
-	const digits = phoneNumber.replace(/\D/g, '');
-
-	// 숫자만 있는 문자열을 (000-0000-0000) 형태로 변환
-	const formatted = digits.replace(/(\d{3})(\d{4})(\d{4})/, '($1-$2-$3)');
-
-	return formatted;
+export const formatPhoneNumber = value => {
+	// 숫자만 추출하고 최대 11자리까지만 허용
+	const digits = value.replace(/\D/g, '').slice(0, 11);
+	const match = digits.match(/^(\d{3})(\d{4})(\d{4})$/);
+	if (match) {
+		return `${match[1]}-${match[2]}-${match[3]}`;
+	}
+	return value;
 };
 
 /**
