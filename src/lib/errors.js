4bbbc9ebd90c9385a0eb5c73fc695b98f41b3fed
aoqlsdl <hyeeev@gmail.com>
2024-03-17 09:45:04 +0900

    ✨feat: api 연동 세팅

---
+++ src/lib/errors.js
@@ -0,0 +1,8 @@
+const handleAxiosError = e => {
+	const { code, message } = e.detail;
+	if (code === 404) {
+		navigate('/wing/recommend'); // 404 에러 시 리다이렉트
+	} else {
+		console.error(message); // 다른 에러 메시지를 콘솔에 출력
+	}
+};
